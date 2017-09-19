b9311cc65caa49567d9d97ee28a98800d83e266c
IgorS <amsterdam1611@gmail.com>
2017-09-19 23:37:59 +0400

    Eslint setup

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.post('/api/auth', (req, res) => {
   res.status(400).json({ errors: { global: 'Invalid credentials' } });
-})
+});
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(8080, () => console.log('Running on localhost:8080'))
+app.listen(8080, () => console.log('Running on localhost:8080'));
